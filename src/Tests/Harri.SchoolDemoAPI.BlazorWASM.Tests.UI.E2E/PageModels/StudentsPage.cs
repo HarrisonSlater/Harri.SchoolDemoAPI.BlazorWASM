425d91b02279e0092ec6c6dbc3f981806249d07c
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2024-05-18 11:00:47 +1000

    Refactor initial e2e test to use Page model

---
+++ src/Tests/Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E/PageModels/StudentsPage.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E.PageModels
+{
+    public class StudentsPage
+    {
+        private readonly IPage _page;
+        private readonly string _baseUrl;
+
+        public ILocator RowsDisplayedLocator => _page.Locator(".mud-table-pagination-select");
+        public ILocator IdDataCellsLocator => _page.Locator("td[data-label=\"SId\"]");
+        public ILocator NameDataCellsLocator => _page.Locator("td[data-label=\"Name\"]");
+        public ILocator GPADataCellsLocator => _page.Locator("td[data-label=\"GPA\"]");
+
+
+        public PaginationActions Pagination { get; set; }
+        public StudentsPage(IPage page, string baseUrl)
+        {
+            _page = page;
+            _baseUrl = baseUrl;
+            Pagination = new PaginationActions(_page);
+        }
+
+        public async Task<int> GetRowsDisplayed()
+        { 
+            var rowsDisplayed = await RowsDisplayedLocator.TextContentAsync();
+
+            rowsDisplayed.Should().NotBeNull();
+
+            var rowsDisplayedInt = int.Parse(rowsDisplayed!);
+            return rowsDisplayedInt;
+        }
+
+        public async Task<IReadOnlyList<string>> AssertRowsAndGetCellData(ILocator locator)
+        {
+            await Assertions.Expect(locator).ToHaveCountAsync(await GetRowsDisplayed());
+            var pageCellData = await locator.AllTextContentsAsync();
+            pageCellData.Should().AllSatisfy(x => x.Should().NotBeEmpty());
+
+            return pageCellData;
+        }
+
+        public async Task GoTo()
+        {
+            await _page.GotoAsync(_baseUrl + "/students");
+        }
+
+        public async Task NavigateTo()
+        {
+            var studentsNavLink = _page.GetByRole(AriaRole.Link, new() { Name = "Students" });
+
+            await studentsNavLink.ClickAsync();
+
+            await Assertions.Expect(_page).ToHaveURLAsync(new Regex(".*students/page/1"));
+        }
+    }
+
+    public class PaginationActions
+    {
+        private readonly IPage _page;
+
+        public PaginationActions(IPage page)
+        {
+            _page = page;
+        }
+
+        public ILocator PaginationActionsLocator => _page.Locator(".mud-table-pagination-actions");
+
+        public ILocator GoToFirstPageButton => PaginationActionsLocator.Locator("button").Nth(0);
+        public ILocator PreviousPageButton => PaginationActionsLocator.Locator("button").Nth(1);
+        public ILocator NextPageButton => PaginationActionsLocator.Locator("button").Nth(2);
+        public ILocator GoToLastPageButton => PaginationActionsLocator.Locator("button").Nth(3);
+    }
+}
--- src/Tests/Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E/StudentsTests.cs
+++ src/Tests/Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E/StudentsTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E.PageModels;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
 using System;
@@ -15,75 +16,55 @@ namespace Harri.SchoolDemoAPI.BlazorWASM.Tests.UI.E2E
     public class StudentsTests : PageTest
     {
         private const string SchoolDemoAdminUrl = "https://localhost:7144";
+        private StudentsPage StudentsPage { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            StudentsPage = new StudentsPage(base.Page, SchoolDemoAdminUrl);
+        }
 
         [Test]
         public async Task CanViewAllStudents_MultiplePages()
         {
             await Page.GotoAsync(SchoolDemoAdminUrl);
 
-            var studentsNavLink = Page.GetByRole(AriaRole.Link, new() { Name = "Students" });
+            await StudentsPage.NavigateTo();
 
-            await studentsNavLink.ClickAsync();
-
-            await Expect(Page).ToHaveURLAsync(new Regex(".*students/page/1"));
-
-            var defaultRows = await Page.Locator(".mud-table-pagination-select").TextContentAsync();
-
-            defaultRows.Should().NotBeNull();
-            var defaultRowsInt = int.Parse(defaultRows);
+            var names = StudentsPage.NameDataCellsLocator;
+            var sIds = StudentsPage.IdDataCellsLocator;
+            var rowsDisplayed = await StudentsPage.GetRowsDisplayed();
 
             var rows = Page.GetByRole(AriaRole.Row);
-            await Expect(rows).ToHaveCountAsync(defaultRowsInt + 2); // + 2 includes header row and footer
+            await Expect(rows).ToHaveCountAsync(rowsDisplayed + 2); // + 2 includes header row and footer
 
-            var sIds = Page.Locator("td[data-label=\"SId\"]");
-            await Expect(sIds).ToHaveCountAsync(defaultRowsInt);
-            var page1Ids = await sIds.AllTextContentsAsync();
-            page1Ids.Should().AllSatisfy(x => x.Should().NotBeEmpty());
+            await StudentsPage.AssertRowsAndGetCellData(sIds);
+            var page1Names = await StudentsPage.AssertRowsAndGetCellData(names);
 
-            var names = Page.Locator("td[data-label=\"Name\"]");
-            await Expect(names).ToHaveCountAsync(defaultRowsInt);
-            var page1Names = await names.AllTextContentsAsync();
-            page1Names.Should().AllSatisfy(x => x.Should().NotBeEmpty());
+            await Expect(StudentsPage.Pagination.GoToFirstPageButton).ToBeDisabledAsync();
+            await Expect(StudentsPage.Pagination.PreviousPageButton).ToBeDisabledAsync();
 
-            var paginationActions = Page.Locator(".mud-table-pagination-actions");
+            await StudentsPage.Pagination.NextPageButton.ClickAsync();
 
-            var gotoFirstButton = paginationActions.Locator("button").Nth(0);
-            var previousButton = paginationActions.Locator("button").Nth(1);
-            var nextButton = paginationActions.Locator("button").Nth(2);
-            var gotoLastButton = paginationActions.Locator("button").Nth(3);
+            var page2Names = await StudentsPage.AssertRowsAndGetCellData(names);
+            page2Names.Should().NotBeEquivalentTo(page1Names); // Assert content different from page 1
 
-            (await gotoFirstButton.IsDisabledAsync()).Should().BeTrue();
-            (await previousButton.IsDisabledAsync()).Should().BeTrue();
+            await StudentsPage.Pagination.PreviousPageButton.ClickAsync();
 
-            await nextButton.ClickAsync();
+            var page1AgainNames = await StudentsPage.AssertRowsAndGetCellData(names);
+            page1AgainNames.Should().BeEquivalentTo(page1Names); // Assert content same from page 1
 
-            await Expect(names).ToHaveCountAsync(defaultRowsInt);
-            var page2Names = await names.AllTextContentsAsync();
-            page2Names.Should().AllSatisfy(x => x.Should().NotBeEmpty());
-            //Assert content different from page 1
-            page2Names.Should().NotBeEquivalentTo(page1Names);
+            await StudentsPage.Pagination.GoToLastPageButton.ClickAsync();
 
-            await previousButton.ClickAsync();
-
-            await Expect(names).ToHaveCountAsync(defaultRowsInt);
-            var page1AgainNames = await names.AllTextContentsAsync();
-            page1AgainNames.Should().AllSatisfy(x => x.Should().NotBeEmpty());
-            //Assert content same from page 1
-            page1AgainNames.Should().BeEquivalentTo(page1Names);
-
-            await gotoLastButton.ClickAsync();
-            //Assert content different from page 1
             var lastPageNames = await names.AllTextContentsAsync();
             lastPageNames.Should().HaveCountGreaterThan(0);
-            lastPageNames.Should().NotBeEquivalentTo(page1Names);
+            lastPageNames.Should().NotBeEquivalentTo(page1Names); // Assert content different from page 1
 
-            await gotoFirstButton.ClickAsync();
 
-            await Expect(names).ToHaveCountAsync(defaultRowsInt);
-            var page1AgainNames2 = await names.AllTextContentsAsync();
-            page1AgainNames2.Should().AllSatisfy(x => x.Should().NotBeEmpty());
-            //Assert content same from page 1
-            page1AgainNames2.Should().BeEquivalentTo(page1Names);
+            await StudentsPage.Pagination.GoToFirstPageButton.ClickAsync();
+
+            var page1AgainNames2 = await StudentsPage.AssertRowsAndGetCellData(names);
+            page1AgainNames2.Should().BeEquivalentTo(page1Names); // Assert content same from page 1
         }
     }
 }
