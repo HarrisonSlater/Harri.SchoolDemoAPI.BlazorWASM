fa504dce084d72432cfe35a64f4305141588edf9
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2024-06-18 08:01:26 +1000

    Clean up EditStudentForm.razor, Add EditSuccessMessage to Students.razor

---
+++ src/Harri.SchoolDemoAPI.BlazorWASM/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace Harri.SchoolDemoAPI.BlazorWASM
+{
+    public static class Constants
+    {
+        public static class QueryString
+        {
+            public const string SuccessId = "successId";
+            public const string EditSuccessId = "editSuccessId";
+        }
+    }
+}
