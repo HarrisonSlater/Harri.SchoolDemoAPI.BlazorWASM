dea660c9f29ab404773a983a5cd8e0970183c3df
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2025-01-10 10:30:25 +1000

    Move int parsing to helper,

---
+++ src/Harri.SchoolDemoAPI.BlazorWASM/Helpers/InputHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Harri.SchoolDemoAPI.BlazorWASM.Helpers
+{
+    public static class InputHelper
+    {
+        /// <returns>Null when unable to parse</returns>
+        public static int? ParseInt(string str)
+        {
+            int i;
+            if (int.TryParse(str, out i))
+            {
+                return i;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
