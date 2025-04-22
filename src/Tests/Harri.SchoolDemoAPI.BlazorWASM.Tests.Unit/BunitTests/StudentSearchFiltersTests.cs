b6ef1da13982a4de958cdacee1770568f09cd0ae
HarrisonSlater <32419991+HarrisonSlater@users.noreply.github.com>
2025-04-22 10:49:05 +1000

    Add parsing test cases to StudentSearchFilterTests.cs

--- src/Tests/Harri.SchoolDemoAPI.BlazorWASM.Tests.Unit/BunitTests/StudentSearchFiltersTests.cs
+++ src/Tests/Harri.SchoolDemoAPI.BlazorWASM.Tests.Unit/BunitTests/StudentSearchFiltersTests.cs
@@ -34,6 +34,7 @@ namespace Harri.SchoolDemoAPI.BlazorWASM.Tests.Unit.BunitTests
         {
             var c = RenderComponent<PropertyColumn<T1, T2>>(parameters => parameters.Add(p => p.Property, e)).Instance;
 
+
             return c;
         }
 
@@ -47,9 +48,6 @@ namespace Harri.SchoolDemoAPI.BlazorWASM.Tests.Unit.BunitTests
                 new FilterDefinition<StudentDto>() { Column = RenderPropertyColumn<StudentDto, decimal?>((StudentDto x) => x.GPA)},
             };
 
-            //var c = RenderComponent<PropertyColumn<StudentDto, decimal?>>(parameters => parameters.Add(p => p.Property, x => x.SId)).Instance;
-
-
             // Act
             _studentSearchFilters = new StudentSearchFilters(filterDefinitions);
 
@@ -62,6 +60,42 @@ namespace Harri.SchoolDemoAPI.BlazorWASM.Tests.Unit.BunitTests
             _studentSearchFilters.NameFilter.Column.PropertyName.Should().Be("Name");
             _studentSearchFilters.SIdFilter.Column.PropertyName.Should().Be("SId");
             _studentSearchFilters.GPAFilter.Column.PropertyName.Should().Be("GPA");
+        }
+
+        private static IEnumerable<TestCaseData> ShouldParseValuesTestCases()
+        {
+            // New student
+            yield return new TestCaseData(null, null, null, null, null);
+            yield return new TestCaseData("Test Student", null, null, null, null);
+            yield return new TestCaseData("Test Student", 1d, 1, null, null);
+            yield return new TestCaseData("Test Student", 123d, 123, null, null);
+            yield return new TestCaseData("Test Student", 123d, 123, 3.89d, 3.89m);
+        }
+
+        [TestCaseSource(nameof(ShouldParseValuesTestCases))]
+        public void StudentSearchFilters_ShouldParseValuesCorrectly(string? name, double? sid, int? expectedParsedSId, double? gpa, decimal? expectedParsedGPA)
+        {
+            // Arrange
+            var filterDefinitions = (ICollection<IFilterDefinition<StudentDto>>)new List<IFilterDefinition<StudentDto>>() {
+                new FilterDefinition<StudentDto>() { Value = name, Column = RenderPropertyColumn<StudentDto, string>((StudentDto x) => x.Name)},
+                new FilterDefinition<StudentDto>() { Value = sid, Column = RenderPropertyColumn<StudentDto, int?>((StudentDto x) => x.SId)},
+                new FilterDefinition<StudentDto>() { Value = gpa, Column = RenderPropertyColumn<StudentDto, decimal?>((StudentDto x) => x.GPA)},
+            };
+
+            //c.FilterContext.FilterDefinition.Value = ;
+
+            // Act
+            _studentSearchFilters = new StudentSearchFilters(filterDefinitions);
+
+            // Assert
+
+            _studentSearchFilters.NameFilter.Should().NotBeNull();
+            _studentSearchFilters.SIdFilter.Should().NotBeNull();
+            _studentSearchFilters.GPAFilter.Should().NotBeNull();
+
+            _studentSearchFilters.ParsedNameFilter.Should().Be(name);
+            _studentSearchFilters.ParsedSIdFilter.Should().Be(expectedParsedSId);
+            _studentSearchFilters.ParsedGPAFilter.Should().Be(expectedParsedGPA);
         }
     }
 }
